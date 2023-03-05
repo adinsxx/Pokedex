7659424aa14f74fc4ca669884be0a0cd994ef2a2
Connor <c.mccurrie@gmail.com>
2023-03-05 16:47:54 -0600

    Implementing pokemon api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import type { PageData } from "./$types";
+import { generations } from "./generations";
+
+
+
+ export let data: PageData;
+</script>
+
+{#each data.monsters as monster (monster.url)}
+    <h1>{monster.name}</h1>
+{/each}
+
+{#each generations as generation (generation.id)}
+<h1>{generation.name}</h1>
+<p>Games: {generation.games.join(', ')}</p>
+<p>Main region: {generation.main_region}</p>
+{/each}
+<style></style>
